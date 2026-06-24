02499382d816598de932c229efa997959ccbd974
Riddhi Pankhade <riddhi.pankhade@gmail.com>
2026-06-24 13:46:09 +0530

    API key variable

--- Backend/pipeline/metadata.py
+++ Backend/pipeline/metadata.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 
 # ---------- setup Gemini ----------
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = os.getenv("GeminiAPIKey")
 
 if not api_key:
     raise ValueError("GeminiAPIKey not found in environment")
